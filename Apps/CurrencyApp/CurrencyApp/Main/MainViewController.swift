613141c91b60f762b1431a9d5210d0ddd2157e06
Mohamed Elbana <m7amdelbana@gmail.com>
2023-10-14 00:02:01 +0300

    Add CurrencyApp

---
+++ Apps/CurrencyApp/CurrencyApp/Main/MainViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MainViewController.swift
+//  CurrencyApp
+//
+//  Created by Mohamed Elbana on 13/10/2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var amountTextFiled: UITextField!
+    @IBOutlet weak var converedAmountLabel: UILabel!
+    @IBOutlet weak var convertButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func actionConvert(_ sender: UIButton) {
+        /*
+        let amount: String = amountTextFiled.text ?? ""
+        if !amount.isEmpty {
+            let usdAmount = (Double(amount) ?? 0.0) * 30.5
+            converedAmountLabel.text = "\(usdAmount) USD"
+        }
+        */
+        
+        guard let amount = amountTextFiled.text,
+              !amount.isEmpty else {
+            converedAmountLabel.text = "Enter EGP amount"
+            converedAmountLabel.textColor = .systemOrange
+            return
+        }
+        
+        guard let egpAmount = Double(amount) else {
+            converedAmountLabel.text = "Enter correct EGP amount"
+            converedAmountLabel.textColor = .systemRed
+            return
+        }
+        
+        let usdAmount = egpAmount * 30.5
+        converedAmountLabel.text = "\(usdAmount) USD"
+        converedAmountLabel.textColor = .systemGreen
+    }
+}
+
